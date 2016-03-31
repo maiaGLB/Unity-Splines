d6f0506df4b1196e8b6cdc7aa515577040e4cc8f
Maia Cordero <maiacordero@gmail.com>
2016-03-31 14:02:32 -0300

    WIP Catmull-Rom following this tutorial http://www.habrador.com/tutorials/catmull-rom-splines/

---
+++ Splines/Assets/Scripts/SplineFollowerCatmull.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplineFollowerCatmull : MonoBehaviour
+{
+    //[SerializeField] private BezierCurve _spineScoreBoard;
+    [SerializeField] private float _duration;
+
+    private CameraController _camera;
+    private float _gameStartTime;
+
+    protected void Start()
+    {
+        _gameStartTime = Time.time;
+        _camera = this.GetComponent<CameraController>();
+    }
+
+    protected void Update()
+    {
+        float delta = Time.time - _gameStartTime;
+        
+        float t = delta / _duration;
+
+        //Vector3 v = _spineScoreBoard.GetPointAt(t);
+        //this.transform.position = v;
+    }
+}
